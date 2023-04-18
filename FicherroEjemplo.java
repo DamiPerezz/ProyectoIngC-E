2d1a9cf375ecdcf8dee8b8297e53df2d85d722c1
Jachpi <121670083+Jachpi@users.noreply.github.com>
2023-04-18 09:02:29 +0200

    t

--- FicherroEjemplo.java
+++ FicherroEjemplo.java
@@ -1,5 +1,52 @@
-public class FicherroEjemplo {
-//HMaria estuvo aquí
+import com.google.gson.Gson;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FicherroEjemplo<var> {
+    // HMaria estuvo aquí
     // nada que ver aquídddd
 
+
+  
+        public static void main(String[] args) {
+            Gson gson = new Gson();
+            try (FileReader reader = new FileReader("datos.json")) {
+                // Leer el archivo JSON en un objeto Java
+                Data data = gson.fromJson(reader, Data.class);
+                System.out.println(data);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+      }
+    
+
+
+}
+
+
+class Data {
+    private int id;
+    private String nombre;
+    private String descripcion;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
 }
