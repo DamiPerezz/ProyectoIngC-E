bd90c701f53f263d21f730c786739f679de24e3e
mariajuarezmo <200039@iesmargaritasalas.com>
2023-05-20 09:21:15 +0200

    Update Menu.java

--- Proyres/ProysRes/src/Model/Menu.java
+++ Proyres/ProysRes/src/Model/Menu.java
@@ -1,125 +1,230 @@
 package Model;
-
-
-import java.io.FileWriter;
-import java.io.IOException;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Menu {
+    public static void main(String[] args) {
+        // Crear los 7 menús distintos
+        List<JSONObject> menus = new ArrayList<>();
 
-	public String precio;
-	public String[] platos;
-	
-	public Menu(String precio, String[] platos) {
-		super();
-	this.precio = precio;
-	this.platos = platos;
-	
-	
-	}
-	 public static void main(String[] args) {
+        // Lunes
+        List<String> platosLunes = new ArrayList<>();
+        platosLunes.add("Salmorejo");
+        platosLunes.add("Empanada de atún");
+        platosLunes.add("Bowl de frutas");
 
+        // Ingredientes para el Salmorejo
+        List<String> ingredientesSalmorejo = Arrays.asList("Tomate", "Pan", "Pepino", "Pimiento");
 
-	    
+        // Ingredientes para la Empanada de atún
+        List<String> ingredientesEmpanadaAtun = Arrays.asList("Hojaldre", "Tomate frito", "Atún", "Huevo cocido");
 
-	      // Crear un objeto JSON para el primer menú
-	      JSONObject menu1 = new JSONObject();
-	      JSONArray platos1 = new JSONArray();
-	      platos1.put("Salmorejo");
-	      platos1.put("Empanada de atún");
-	      platos1.put("Bowl con fruta");
-	      menu1.put("platos", platos1);
-	      menu1.put("precio", "12 euros");
+        // Ingredientes para el Bowl de frutas
+        List<String> ingredientesBowlFrutas = Arrays.asList("Fresa", "Melón", "Plátano", "Naranja", "Manzana");
 
+        JSONObject menuLunes = crearMenu("Lunes", platosLunes, ingredientesSalmorejo, ingredientesEmpanadaAtun, ingredientesBowlFrutas);
+        menus.add(menuLunes);
 
+        // Martes
+        List<String> platosMartes = new ArrayList<>();
+        platosMartes.add("Menestra de verduras");
+        platosMartes.add("Fingers de pollo");
+        platosMartes.add("Arroz con leche");
 
-	      // Crear un objeto JSON para el segundo menú
-	      JSONObject menu2 = new JSONObject();
-	      JSONArray platos2 = new JSONArray();
-	      platos2.put("Menestra de verduras");
-	      platos2.put("Fingers de pollo");
-	      platos2.put("Arroz con leche");
-	      menu2.put("platos", platos2);
-	      menu2.put("precio", "12 euros");
+        // Ingredientes para la Menestra de verduras
+        List<String> ingredientesMenestraVerduras = Arrays.asList("Calabacín", "Zanahoria", "Cebolla", "Pepino");
 
+        // Ingredientes para los Fingers de pollo
+        List<String> ingredientesFingersPollo = Arrays.asList("Pollo", "Pan rallado", "Huevo");
 
-	     
-	      
-	      // Crear un objeto JSON para el segundo menú
-	      JSONObject menu3 = new JSONObject();
-	      JSONArray platos3 = new JSONArray();
-	      platos3.put("Sopa");
-	      platos3.put("Cocido");
-	      platos3.put("Tarta de queso");
-	      menu3.put("platos", platos3);
-	      menu3.put("precio", "12 euros");
+        // Ingredientes para el Arroz con leche
+        List<String> ingredientesArrozLeche = Arrays.asList("Arroz", "Leche", "Azúcar", "Canela");
 
-	      
-	   // Crear un objeto JSON para el cuarto menú
-	      JSONObject menu4 = new JSONObject();
-	      JSONArray platos4 = new JSONArray();
-	      platos4.put("Ensalada");
-	      platos4.put("Lubina al horno");
-	      platos4.put("Brownie");
-	      menu4.put("platos", platos4);
-	      menu4.put("precio", "12 euros");
+        JSONObject menuMartes = crearMenu("Martes", platosMartes, ingredientesMenestraVerduras, ingredientesFingersPollo, ingredientesArrozLeche);
+        menus.add(menuMartes);
 
+        
+        // Miercoles
+        List<String> platosMiercoles = new ArrayList<>();
+        platosMartes.add("Sopa");
+        platosMartes.add("Cocido");
+        platosMartes.add("Tarta de queso");
 
-	      
-	      // Crear un objeto JSON para el quinto menú
-	      JSONObject menu5 = new JSONObject();
-	      JSONArray platos5 = new JSONArray();
-	      platos5.put("Puré de calabacín");
-	      platos5.put("Solomillo con patatas");
-	      platos5.put("Bola de helado");
-	      menu5.put("platos", platos5);
-	      menu5.put("precio", "12 euros");
+        // Ingredientes para la sopa
+        List<String> ingredientesSopa = Arrays.asList("Caldo de verduras", "Fideos", "Pollo");
 
+        // Ingredientes para el cocido
+        List<String> ingredientesCocido = Arrays.asList("Garbanzos", "Chorizo", "Tocino", "Repollo");
 
-	      
-	      // Crear un objeto JSON para el sexto menú
-	      JSONObject menu6 = new JSONObject();
-	      JSONArray platos6 = new JSONArray();
-	      platos6.put("Puré de calabaza");
-	      platos6.put("Merluza en salsa");
-	      platos6.put("Flan");
-	      menu6.put("platos", platos6);
-	      menu6.put("precio", "12 euros");
+        // Ingredientes para la tarta de queso
+        List<String> ingredientesTartadeQueso= Arrays.asList("Queso crema", "Leche", "Azúcar", "Mermelada"," Galletas", "Huevos");
 
+        JSONObject menuMiercoles = crearMenu("Miércoles", platosMiercoles, ingredientesSopa, ingredientesCocido, ingredientesTartadeQueso);
+        menus.add(menuMiercoles);
 
-	      // Crear un objeto JSON para el septimo menú
-	      JSONObject menu7 = new JSONObject();
-	      JSONArray platos7 = new JSONArray();
-	      platos7.put("Judias con huevo y jamón");
-	      platos7.put("San jacobo");
-	      platos7.put("Fresas con nata");
-	      menu7.put("platos", platos7);
-	      menu7.put("precio", "12 euros");
+        
+        // Jueves
+        List<String> platosJueves = new ArrayList<>();
+        platosMartes.add("Ensalada");
+        platosMartes.add("Lubina al horno");
+        platosMartes.add("Brownie");
 
-	      JSONArray menuArray = new JSONArray();
-	        menuArray.put(menu1);
-	        menuArray.put(menu2);
-	        menuArray.put(menu3);
-	        menuArray.put(menu4);
-	        menuArray.put(menu5);
-	        menuArray.put(menu6);
-	        menuArray.put(menu7);
+        // Ingredientes para la ensalada
+        List<String> ingredientesEnsalada = Arrays.asList("Brotes de espinacas", "Zanahoria", "Tomate", "Pepino", "Maíz", "Remolacha", "Atún");
 
-	      try (FileWriter file = new FileWriter("menus.json")) {
-		         file.write(menuArray.toString());
-		       
-		         System.out.println("Menús guardados en el archivo menus.json");
-		      } catch (IOException e) {
-		         e.printStackTrace();
-		      }
-	      
-	      
-	      
-	   }
+        // Ingredientes para la lubina al horno
+        List<String> ingredientesLubinaAlHorno = Arrays.asList("Lubina", "Patata", "Cebolla", "Salsa peregíl");
 
-	
-	}
+        // Ingredientes para el brownie
+        List<String> ingredientesBrownie = Arrays.asList("Chocolate", "Leche", "Azúcar", "Huevos", "Harina", "Nueces");
 
+        JSONObject menuJueves = crearMenu("Jueves", platosJueves, ingredientesEnsalada, ingredientesLubinaAlHorno, ingredientesBrownie);
+        menus.add(menuJueves);
 
+        
+        // Viernes
+        List<String> platosViernes = new ArrayList<>();
+        platosMartes.add("Puré de calabacín");
+        platosMartes.add("Solomillo con patatas");
+        platosMartes.add("Bola de helado");
 
+        // Ingredientes para el puré de calabacín
+        List<String> ingredientesPureDeCalabacin = Arrays.asList("Calabacín", "Zanahoria", "Cebolla", "Patata", " Caldo de verduras");
+
+        // Ingredientes para el solomillo con patatas
+        List<String> ingredientesSolomilloConPatatas = Arrays.asList("Solomillo", "Patata", "Pimiento", "Salsa de vino");
+
+        // Ingredientes para la bola de helado
+        List<String> ingredientesBolaDeHelado = Arrays.asList( "Leche", "Azúcar", "Canela", "Vainilla");
+
+        JSONObject menuViernes = crearMenu("Viernes", platosViernes, ingredientesPureDeCalabacin, ingredientesSolomilloConPatatas, ingredientesBolaDeHelado);
+        menus.add(menuViernes);
+
+        
+        // Sábado
+        List<String> platosSabado = new ArrayList<>();
+        platosMartes.add("Puré de calabaza");
+        platosMartes.add("Merluza en salsa");
+        platosMartes.add("Flan");
+
+        // Ingredientes para el puré de calabaza
+        List<String> ingredientesPureDeCalabaza = Arrays.asList("Calabacín", "Zanahoria", "Cebolla", "Calabaza", "Caldo de verudras");
+
+        // Ingredientes para la merluza en salsa
+        List<String> ingredientesMerluzaEnSalsa = Arrays.asList("Merluza", "Almejas", "Salsa de marisco", "Chipirones");
+
+        // Ingredientes para el flan
+        List<String> ingredientesFlan = Arrays.asList("Huevo", "Leche", "Azúcar");
+
+        JSONObject menuSabado = crearMenu("Sábado", platosSabado, ingredientesPureDeCalabaza, ingredientesMerluzaEnSalsa, ingredientesFlan);
+        menus.add(menuSabado);
+
+        
+        // Domingo
+        List<String> platosDomingo = new ArrayList<>();
+        platosMartes.add("Judías con huevo y jamón");
+        platosMartes.add("San jacobo");
+        platosMartes.add("Fresas con nata");
+
+        // Ingredientes para las jadías con huevo y jamón
+        List<String> ingredientesJudiasHuevoJamon = Arrays.asList("Judías", "Jamón serrano", "Huevo cocido");
+
+        // Ingredientes para el San jacobo
+        List<String> ingredientesSanJacobo = Arrays.asList("Jamón cocido", "Queso", "Pan rallado");
+
+        // Ingredientes para las fresas con nata
+        List<String> ingredientesFresasConNata = Arrays.asList("Fresas", "Nata montada");
+
+        JSONObject menuDomingo = crearMenu("Domingo", platosDomingo, ingredientesJudiasHuevoJamon, ingredientesSanJacobo, ingredientesFresasConNata);
+        menus.add(menuDomingo);
+
+  
+
+        // Crear el JSONArray con todos los menús
+        JSONArray jsonArray = new JSONArray(menus);
+
+        // Guardar los menús en un archivo JSON
+        guardarMenusEnArchivo(jsonArray);
+    }
+
+    private static JSONObject crearMenu(String diaSemana, List<String> platos, List<String> ingredientesPlato1, List<String> ingredientesPlato2, List<String> ingredientesPlato3) {
+        JSONObject menu = new JSONObject();
+        menu.put("dia_semana", diaSemana);
+
+        // Crear una lista de platos
+        List<JSONObject> platosJson = new ArrayList<>();
+        for (int i = 0; i < platos.size(); i++) {
+            String plato = platos.get(i);
+            List<String> ingredientes = new ArrayList<>();
+            if (plato.equals("Salmorejo")) {
+                ingredientes = ingredientesPlato1;
+            } else if (plato.equals("Empanada de atún")) {
+                ingredientes = ingredientesPlato2;
+            } else if (plato.equals("Bowl de frutas")) {
+                ingredientes = ingredientesPlato3;
+            } else if (plato.equals("Menestra de verduras")) {
+                ingredientes = ingredientesPlato1;
+            } else if (plato.equals("Fingers de pollo")) {
+                ingredientes = ingredientesPlato2;
+            } else if (plato.equals("Arroz con leche")) {
+                ingredientes = ingredientesPlato3;
+            }else if (plato.equals("Sopa")) {
+                ingredientes = ingredientesPlato1;
+            } else if (plato.equals("Cocido")) {
+                ingredientes = ingredientesPlato2;
+            } else if (plato.equals("Tarta de queso")) {
+                ingredientes = ingredientesPlato3;
+            }else if (plato.equals("Ensalada")) {
+                ingredientes = ingredientesPlato1;
+            } else if (plato.equals("Lubina al horno")) {
+                ingredientes = ingredientesPlato2;
+            } else if (plato.equals("Brownie")) {
+                ingredientes = ingredientesPlato3;
+            }else if (plato.equals("Puré de calabacín")) {
+                ingredientes = ingredientesPlato1;
+            } else if (plato.equals("Solomillo con patatas")) {
+                ingredientes = ingredientesPlato2;
+            } else if (plato.equals("Bola de helado")) {
+                ingredientes = ingredientesPlato3;
+            }else if (plato.equals("Puré de calabaza")) {
+                ingredientes = ingredientesPlato1;
+            } else if (plato.equals("Merluza en salsa")) {
+                ingredientes = ingredientesPlato2;
+            } else if (plato.equals("Flan")) {
+                ingredientes = ingredientesPlato3;
+            }else if (plato.equals("Judías con huevo y jamón")) {
+                ingredientes = ingredientesPlato1;
+            } else if (plato.equals("San jacobo")) {
+                ingredientes = ingredientesPlato2;
+            } else if (plato.equals("Fresas con nata")) {
+                ingredientes = ingredientesPlato3;
+            }
+            platosJson.add(crearPlato(plato, ingredientes));
+        }
+
+        menu.put("platos", new JSONArray(platosJson));
+        return menu;
+    }
+
+    private static JSONObject crearPlato(String nombre, List<String> ingredientes) {
+        JSONObject plato = new JSONObject();
+        plato.put("nombre", nombre);
+        plato.put("ingredientes", new JSONArray(ingredientes));
+        return plato;
+    }
+
+    private static void guardarMenusEnArchivo(JSONArray jsonArray) {
+        try (FileWriter file = new FileWriter("menus.json")) {
+            file.write(jsonArray.toString());
+            System.out.println("Los menús se han guardado correctamente en el archivo 'menus.json'");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
